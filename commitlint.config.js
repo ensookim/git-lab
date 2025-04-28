5716c82fec75ef0e745e642e9e4d5dca6a01b6f0
Eunsoo Kim <7.8204620u@gmail.com>
2025-04-28 16:36:33 +0900

    feat: Add page

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,9 +1,1 @@
-module.exports = {
-  extends: ['@commitlint/config-conventional'],
-  rules: {
-    // 커밋 메시지에서 타입을 반드시 포함하도록 설정
-    'type-enum': [2, 'always', ['feat', 'fix', 'docs']],
-    // 커밋 메시지 길이를 100자로 제한
-    'header-max-length': [2, 'always', 100],
-  },
-};
+module.exports = { extends: ['@commitlint/config-conventional'] };
